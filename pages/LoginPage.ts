bec6233b06a149014f226c4df4c2fd31b3494b8f
Mickooo17 <63809754+Mickooo17@users.noreply.github.com>
2025-10-02 10:57:44 +0200

    added new test

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -4,16 +4,29 @@ export class LoginPage {
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly submitButton: Locator;
+  readonly loginErrorMessage: Locator;
 
   constructor(private page: Page) {
     this.usernameInput = page.locator('#username');
     this.passwordInput = page.locator('#password');
     this.submitButton = page.locator('button[id="restore-password-button"]');
+    this.loginErrorMessage = page.getByText('This field is required.');
   }
 
   async login(username: string, password: string) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.submitButton.click();
+  }
+
+    async validateErrorMessage() {
+
+  const count = await this.loginErrorMessage.count();
+  if (count === 0) {
+    throw new Error('There are no error messages displayed.');
+  }
+  for (let i = 0; i < count; i++) {
+    await this.loginErrorMessage.nth(i).waitFor({ state: 'visible', timeout: 5000 });
+    }
   }
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,3 +4,8 @@ f.test('Verify user is able to login to the page @smoke @regression', async ({ l
   await loginPage.login(f.adminProfessionalUser.username, f.adminProfessionalUser.password);
   await homePage.validateHomePageIsOpened();
 });
+
+f.test('Verify user is not able to login with invalid credentials @regression', async ({ loginPage }) => {
+  await loginPage.login('', '');
+  await loginPage.validateErrorMessage();
+});
